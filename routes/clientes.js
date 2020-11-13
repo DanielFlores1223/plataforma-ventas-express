ba0935503c927a79980846ffd2ed11dc99c3d2e1
DanielFlores1223 <dany-232000@hotmail.com>
2020-11-13 09:20:25 -0600

    avances en clientes y empleados

--- routes/clientes.js
+++ routes/clientes.js
@@ -26,10 +26,20 @@ router.get('/buscar-cli-correo', async(req, res) => {
     const cliente = await Cliente.findOne({ correo: req.body.correo });
     const correo = req.body.correo;
     if (!cliente)
-        return res.status(404).send("aui " + correo);
+        return res.status(404).send(false);
 
     res.status(200).send(cliente);
 });
+
+router.post('/buscar-like', async(req, res) => {
+
+    const clientes = await Cliente.find({ '$or': [{ nombre: { '$regex': req.body.nombre, '$options': 'i' } }, { apellidos: { '$regex': req.body.nombre, '$options': 'i' } }] });
+
+    if (!clientes)
+        return res.status(404).send(false);
+
+    res.status(200).send(clientes);
+})
 
 //Insertar
 router.post('/', async(req, res) => {
@@ -51,5 +61,59 @@ router.post('/', async(req, res) => {
     res.status(200).send(cliente);
 
 });
+
+//modificar perfil
+
+//modificar cliente
+router.put('/', async(req, res) => {
+    const cliente = await Cliente.findOne({ correo: req.body.correo });
+
+    if (!cliente)
+        return res.status(400).send(false);
+
+    clienteMod = await Cliente.findOneAndUpdate({ correo: req.body.correo }, {
+        //campos a modificar
+        nombre: req.body.nombre,
+        apellidos: req.body.apellidos,
+        telefono: req.body.telefono,
+        correo: req.body.correo,
+        tipo: req.body.tipo
+    }, {
+        new: true
+    });
+
+    res.send(clienteMod);
+});
+
+router.put('/modificar-contrasenia', async(req, res) => {
+    const cliente = await Cliente.findOne({ correo: req.body.correo });
+
+    if (!cliente)
+        return res.status(400).send(false);
+
+    const salt = await bcyrpt.genSalt(10);
+    const contraCifrada = await bcyrpt.hash(req.body.contrasenia, salt);
+
+    clienteMod = await Cliente.findOneAndUpdate({ correo: req.body.correo }, {
+        //modificar contraseña
+        contrasenia: contraCifrada
+    }, {
+        new: true
+    });
+    res.send(clienteMod);
+});
+
+//Eliminar
+router.post('/eliminar', async(req, res) => {
+    const cliente = await Cliente.findOne({ correo: req.body.correo });
+
+    if (!cliente)
+        return res.status(400).send(false);
+
+    cliente_eliminado = await Cliente.findOneAndDelete({ correo: req.body.correo });
+
+    res.send(cliente_eliminado);
+
+})
 
 module.exports = router;
--- routes/empleados.js
+++ routes/empleados.js
@@ -30,19 +30,33 @@ router.post('/buscar-emp-correo', async(req, res) => {
 
     res.status(200).send(empleado);
 });
-/*
-router.get('/:correo', async(req, res) => {
-    const empleado = await Empleado.findOne({ correo: req.params.correo });
 
-    if (!empleado)
+router.post('/buscar-like', async(req, res) => {
+
+    const empleados = await Empleado.find({ '$or': [{ nombre: { '$regex': req.body.nombre, '$options': 'i' } }, { apellidos: { '$regex': req.body.nombre, '$options': 'i' } }] });
+
+    if (!empleados)
         return res.status(404).send(false);
 
-    res.status(200).send(empleado);
-});
-*/
+    res.status(200).send(empleados);
+})
+
 
 //insertar
-router.post('/', async(req, res) => {
+router.post('/', [
+    //validaciones
+    check('nombre').isLength({ min: 1 }),
+    check('apellidos').isLength({ min: 1 }),
+    check('telefono').isLength({ min: 1 }),
+    check('sueldo').isLength({ min: 1 }),
+    check('correo').isLength({ min: 1 }),
+    check('correo').isEmail(),
+], async(req, res) => {
+
+    const errores = validationResult(req);
+
+    if (!errores.isEmpty())
+        return res.status(422).json({ errors: errores.array() });
 
     //encriptacion de la contraseña del empleado
     const salt = await bcyrpt.genSalt(10);
@@ -65,6 +79,61 @@ router.post('/', async(req, res) => {
 });
 
 //modificar perfil
+
+
+//modificar empleado (sin contraseña)
+router.put('/', async(req, res) => {
+    const empleado = await Empleado.findOne({ correo: req.body.correo });
+
+    if (!empleado)
+        return res.status(400).send(false);
+
+
+    empleadoMod = await Empleado.findOneAndUpdate({ correo: req.body.correo }, {
+        //campos a modificar
+        nombre: req.body.nombre,
+        apellidos: req.body.apellidos,
+        telefono: req.body.telefono,
+        sueldo: req.body.sueldo,
+        correo: req.body.correo,
+        fechaNac: req.body.fechaNac,
+        tipo: req.body.tipo
+    }, {
+        new: true
+    });
+
+    res.send(empleadoMod);
+});
+
+router.put('/modificar-contrasenia', async(req, res) => {
+    const empleado = await Empleado.findOne({ correo: req.body.correo });
+
+    if (!empleado)
+        return res.status(400).send(false);
+
+    const salt = await bcyrpt.genSalt(10);
+    const contraCifrada = await bcyrpt.hash(req.body.contrasenia, salt);
+
+    empleadoMod = await Empleado.findOneAndUpdate({ correo: req.body.correo }, {
+        //modificar contraseña
+        contrasenia: contraCifrada
+    }, {
+        new: true
+    });
+    res.send(empleadoMod);
+});
+
+//Eliminar
+router.post('/eliminar', async(req, res) => {
+    const empleado = await Empleado.findOne({ correo: req.body.correo });
+
+    if (!empleado)
+        return res.status(400).send(false);
+
+    empelado_eliminado = await Empleado.findOneAndDelete({ correo: req.body.correo });
+
+    res.send(empleado);
+})
 
 
 module.exports = router;
